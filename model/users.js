fc08bf244334bb564ef2121a77098e818d519879
benznt <tmno2tm@gmail.com>
2018-12-03 23:24:53 +0700

    first commit, add whole project, complete sign-in method

---
+++ model/users.js
@@ -0,0 +1,66 @@
+Users = [
+    {
+        "id": "95ce0f16-6f61-4660-86b8-ac1193fc2011",
+        "full_name": "Darcey Storrock",
+        "phai": "Female",
+        "sdt": "296-274-4357",
+        "mk": "3DYzFKjy29u"
+      }, {
+        "id": "b6d6113d-504b-4b2c-b208-05fc74e51436",
+        "full_name": "Blakelee Oboy",
+        "phai": "Female",
+        "sdt": "673-131-0834",
+        "mk": "h7VJou4gu"
+      }, {
+        "id": "880f564f-e95e-492b-afc3-7aee42b47e78",
+        "full_name": "Liliane Benardet",
+        "phai": "Female",
+        "sdt": "217-200-1287",
+        "mk": "r2EsaQJ"
+      }, {
+        "id": "9d1ef688-8972-4ea4-8dda-efa7db1f1001",
+        "full_name": "Gilemette Mendez",
+        "phai": "Female",
+        "sdt": "467-519-4124",
+        "mk": "wqIT2qf"
+      }, {
+        "full_name": "benznt",
+        "mk": "123"
+      }, {
+        "id": "3134df9a-1493-4b37-8c23-e2ad077cc338",
+        "full_name": "Sandy Handes",
+        "phai": "Male",
+        "sdt": "327-695-6891",
+        "mk": "5J3y9TKV"
+      }, {
+        "id": "8f66d99e-9065-4d23-96fc-3af313f087b3",
+        "full_name": "Ronald Rye",
+        "phai": "Male",
+        "sdt": "163-645-9548",
+        "mk": "BO5f8SYME"
+      }, {
+        "id": "885dbaf0-9e18-4831-a1fc-963a1d2b8002",
+        "full_name": "Harland Basnett",
+        "phai": "Male",
+        "sdt": "395-324-3815",
+        "mk": "Euwy8TdKf9"
+      }, {
+        "id": "744ae3c1-3263-41ef-b891-bc85f339b0eb",
+        "full_name": "Sephira Glanister",
+        "phai": "Female",
+        "sdt": "736-251-4892",
+        "mk": "abRp58plS"
+      }, {
+        "id": "25ebd06a-72fe-46c0-843d-63896b4739a8",
+        "full_name": "Muffin Garmanson",
+        "phai": "Male",
+        "sdt": "129-730-1087",
+        "mk": "QwKYcL"
+      }, {
+        "id": "b4eedf6a-33eb-4125-a0b7-d079f36acff3",
+        "full_name": "Jard Antonov",
+        "phai": "Male",
+        "sdt": "737-500-7152",
+        "mk": "np9ZO0"
+      }
+]
