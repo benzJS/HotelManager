d212fffa3d331066633abed8dd5fa4f33b7a4fef
benznt <tmno2tm@gmail.com>
2018-12-06 11:08:04 +0700

    coding order method (use promise)

--- model/rooms.js
+++ model/rooms.js
@@ -1,8 +1,70 @@
 Rooms = [
-    {
+    /*{
+        id:'1',
         title: 'Ariana',
-        img: '',
-        desc: '',
-        price: ''
-    }
+        img: '../public/images/BWPlus_Astana_Hotel_Kasachstan.jpg',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate praesentium ex dicta, doloribus dolor neque nam quas consequuntur nostrum, porro atque accusantium aspernatur eos ad perspiciatis cumque obcaecati culpa animi.',
+        price: '499'
+    },*/
+    {
+        "id": 1,
+        "title": "Drewry",
+        "desc": "Repair Right Thyroid Artery, Open Approach",
+        "price": 421,
+        "img": "http://dummyimage.com/246x220.jpg/ff4444/ffffff"
+      }, {
+        "id": 2,
+        "title": "West",
+        "desc": "Robotic Assisted Procedure of Upper Extremity, Percutaneous Endoscopic Approach",
+        "price": 438,
+        "img": "http://dummyimage.com/246x220.bmp/dddddd/000000"
+      }, {
+        "id": 3,
+        "title": "Kipling",
+        "desc": "Supplement Right Pelvic Bone with Nonautologous Tissue Substitute, Percutaneous Approach",
+        "price": 389,
+        "img": "http://dummyimage.com/246x220.png/5fa2dd/ffffff"
+      }, {
+        "id": 4,
+        "title": "Southridge",
+        "desc": "Drainage of Left Neck Lymphatic with Drainage Device, Open Approach",
+        "price": 242,
+        "img": "http://dummyimage.com/246x220.bmp/5fa2dd/ffffff"
+      }, {
+        "id": 5,
+        "title": "Morningstar",
+        "desc": "Dilation of Left Common Carotid Artery with Four or More Drug-eluting Intraluminal Devices, Open Approach",
+        "price": 341,
+        "img": "http://dummyimage.com/246x220.jpg/ff4444/ffffff"
+      }, {
+        "id": 6,
+        "title": "Bluestem",
+        "desc": "Bypass Right Axillary Artery to Left Lower Arm Artery with Autologous Arterial Tissue, Open Approach",
+        "price": 277,
+        "img": "http://dummyimage.com/246x220.bmp/dddddd/000000"
+      }, {
+        "id": 7,
+        "title": "Dayton",
+        "desc": "Drainage of Cerebral Meninges, Percutaneous Approach",
+        "price": 476,
+        "img": "http://dummyimage.com/246x220.png/ff4444/ffffff"
+      }, {
+        "id": 8,
+        "title": "Messerschmidt",
+        "desc": "Replacement of Left Thumb Phalanx with Synthetic Substitute, Percutaneous Approach",
+        "price": 418,
+        "img": "http://dummyimage.com/246x220.jpg/dddddd/000000"
+      }, {
+        "id": 9,
+        "title": "Stone Corner",
+        "desc": "Reposition Right Subclavian Artery, Percutaneous Endoscopic Approach",
+        "price": 306,
+        "img": "http://dummyimage.com/246x220.bmp/5fa2dd/ffffff"
+      }, {
+        "id": 10,
+        "title": "Crowley",
+        "desc": "Excision of Cecum, Percutaneous Endoscopic Approach",
+        "price": 292,
+        "img": "http://dummyimage.com/246x220.bmp/5fa2dd/ffffff"
+      }
 ]
